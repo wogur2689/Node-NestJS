5ba28345bcbe2af783c3abb47583fb82cf5e8edc
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2023-07-03 18:03:28 +0900

    devtools 추가 및 입력창 만들기

--- src/main/java/com/example/mail/dto/EmailDto.java
+++ src/main/java/com/example/mail/dto/EmailDto.java
@@ -1,12 +1,11 @@
 package com.example.mail.dto;
 
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailDto {
     private String fromAddress; //내 이메일
@@ -16,15 +15,4 @@ public class EmailDto {
     private String subject; //제목
     private String content; //내용
     private boolean isUseHtmlYn; //메일 형식이 HTML인지 여부
-
-    @Builder
-    public EmailDto(String fromAddress, String toUser, String ccUser, String bccUser, String subject, String content, boolean isUseHtmlYn) {
-        this.fromAddress = fromAddress;
-        this.toUser = toUser;
-        this.ccUser = ccUser;
-        this.bccUser = bccUser;
-        this.subject = subject;
-        this.content = content;
-        this.isUseHtmlYn = isUseHtmlYn;
-    }
 }
